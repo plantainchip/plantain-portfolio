b00201d009d641ba05abf4e6f228f64f0f7b7b94
Maitri <maitrivathada@gmail.com>
2023-06-16 19:31:08 +0000

    updated 2021 art names

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// jQuery("#animated-thumbnails-gallery")
-//   .justifiedGallery({
-//     captions: false,
-//     lastRow: "hide",
-//     rowHeight: 180,
-//     margins: 5
-//   })
-//   .on("jg.complete", function() {
-//     window.lightGallery(
-//       document.getElementById("animated-thumbnails-gallery"),
-//       {
-//         autoplayFirstVideo: false,
-//         pager: false,
-//         galleryId: "nature",
-//         plugins: [lgZoom, lgThumbnail],
-//         mobileSettings: {
-//           controls: false,
-//           showCloseIcon: false,
-//           download: false,
-//           rotate: false
-//         }
-//       }
-//     );
-//   });
-
-
-// var grid = document.querySelector('.grid');
-// if (grid) {
-//   var msnry = new Masonry(grid, {
-//     // options...
-//     itemSelector: '.grid-item',
-//     columnWidth: 200
-//   });
-// }
 
 lightGallery(document.getElementById('lightgallery'), {
   plugins: [lgZoom, lgThumbnail],
